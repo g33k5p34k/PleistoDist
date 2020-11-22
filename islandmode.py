83806e8f4de99afde4a3b70684ca23f5ed9cc938
g33k5p34k <47347170+g33k5p34k@users.noreply.github.com>
2020-11-22 18:14:15 +0800

    Update islandmode.py

--- islandmode.py
+++ islandmode.py
@@ -39,19 +39,15 @@ def islandmode(intervalfile,points):
         #open least overwater distance matrix file and write header
         g = open("output/island_leastdist_interval"+str(i)+".csv","w")
         g.write(header)
-        #h = open("output/islandcentroid_metres_interval"+str(i)+".csv","w")
-        #h.write(header)
         for pf in source: #start of 1st order for-loop for origin points
             #append FID value to first column in row for each distance matrix type
             euccentroid = [str(pf[0])]
             leastdist = [str(pf[0])]
-            #euccentroid_metres = [str(pf[0])]
             for pt in sink: #start of 2nd order for-loop for destination points
                 print("Source point: "+ str(pf[0])+ "; Sink point: " + str(pt[0]))
                 if pf == pt: #set NA value for self-to-self comparisons
                     euccentroid.append("NA")
                     leastdist.append("NA")
-                    #euccentroid_metres.append("NA")
                 else:
                     strSQL1 = """ "FID" = {0}""".format(pf[0]) #define SQL search string for source point
                     strSQL2 = """ "FID" = {0}""".format(pt[0]) #define SQL search string for destination point
@@ -61,7 +57,6 @@ def islandmode(intervalfile,points):
                         print("Point "+str(pf[0])+" is below sea level during interval "+str(i)+", writing distance value of NA")
                         euccentroid.append("NA")
                         leastdist.append("NA")
-                        #euccentroid_metres.append("NA")
                     else:
                         arcpy.SelectLayerByAttribute_management(sourcepoints,"ADD_TO_SELECTION",strSQL2) #if source point is on valid landmass, select destination point in sourcepoints shapefile
                         arcpy.SelectLayerByLocation_management(intervalshp,"INTERSECT",sourcepoints, selection_type = "ADD_TO_SELECTION")
@@ -69,7 +64,6 @@ def islandmode(intervalfile,points):
                             print("Points "+str(pf[0])+" and "+str(pt[0])+" are on the same landmass, printing distance of 0")
                             euccentroid.append(str(0))
                             leastdist.append(str(0))
-                            #euccentroid_metres.append(str(0))
                         else:
                             cursor = arcpy.da.SearchCursor(intervalshp,["SHAPE@XY","SHAPE@"])
                             centroid = []
@@ -78,22 +72,17 @@ def islandmode(intervalfile,points):
                                 centroid.append(a[0])
                                 geometry.append(a[1])
                             eucdist_var = math.sqrt(((centroid[1][0]-centroid[0][0])**2) + ((centroid[1][1]-centroid[0][1])**2))
-                            #eucdist_haversine = haversine(centroid[0][1],centroid[0][0],centroid[1][1],centroid[1][0])
                             leastdist_var = geometry[0].distanceTo(geometry[1])
                             euccentroid.append(str(eucdist_var))
                             leastdist.append(str(leastdist_var))
-                            #euccentroid_metres.append(str(eucdist_haversine))
                             del cursor
             euclideancentroid = ','.join(euccentroid)
             leastdistance = ','.join(leastdist)
-            #euclideanmetres = ','.join(euccentroid_metres)
             f.write(euclideancentroid+"\n")
             g.write(leastdistance+"\n")
-            #h.write(euccentroid_metres+"\n")
             sink.reset()
         f.close()
         g.close()
-        h.close()
         source.reset()
     del source
     del sink
