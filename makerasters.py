819d5d9fe2bef2ed21ce354606564888fd27ed6e
g33k5p34k <47347170+g33k5p34k@users.noreply.github.com>
2020-11-22 17:34:27 +0800

    Add files via upload

--- makerasters.py
+++ makerasters.py
@@ -13,12 +13,18 @@ env.workspace = path
 #activate spatial analyst license
 arcpy.CheckOutExtension("Spatial")
 #define make rasters function
-def makerasters(intervalfile,inputraster):
+def makerasters(intervalfile,inputraster,epsg):
+    spatialref_default = arcpy.SpatialReference(4326) #define the default GEBCO projection
+    spatialref_proj = arcpy.SpatialReference(epsg) #define the projection for the project
     baseraster = arcpy.ASCIIToRaster_conversion(r"input/"+inputraster) #open input ASCII file and convert to raster format
+    arcpy.DefineProjection_management(baseraster,spatialref_default) #set the projection of the raster to default
+    arcpy.ProjectRaster_management(baseraster,"baseraster_projected.tif",spatialref_proj,"BILINEAR") #reproject raster into correct project projection to enable accurate distance measurements
+    inraster = "baseraster_projected.tif" #load new raster
+    arcpy.DefineProjection_management(inraster,spatialref_proj) #set new raster projection
     for i in intervalfile['MeanDepth'].tolist(): #iterate through Mean Depth values (1 per interval)
         x = int(intervalfile[intervalfile['MeanDepth']==i]['Interval']) #pull associated interval value for each Mean Depth value
         print("Generating island raster for interval " +str(x) + ", sea level = " + str(i)) #print status messages
-        outraster = Raster(baseraster) >= i #most important part of this function -- defining land/sea boundary based on interval sea level
+        outraster = Raster(inraster) >= i #most important part of this function -- defining land/sea boundary based on interval sea level
         outraster_reclass = Con(outraster, 1, 100, "value > 0") #convert non-land pixels to value = 100, for least cost distance analysis
         filename = "interval"+str(x) #define output name string
         arcpy.RasterToASCII_conversion(outraster_reclass,"output/raster/"+filename+".asc") #save reclassed raster to ASCII file
