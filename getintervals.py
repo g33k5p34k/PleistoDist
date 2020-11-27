2ec9b432d0d5cb7376fc5659ed66a5fc8ed2e32d
David Tan <g33k5p34k@33.8.126.129.static.m1net.com.sg>
2020-11-27 14:16:24 +0800

    debugged getintervals.py to account for sea levels greater than present day

--- getintervals.py
+++ getintervals.py
@@ -4,7 +4,7 @@ import numpy
 
 def getintervals (sealvl,time,intervals):
 #subset full sea level table based on cutoff time
-    sealvl_subset = sealvl[sealvl.Time <= time]
+    sealvl_subset = sealvl[(sealvl.Time <= time) & (sealvl.Time > 0)] #since T = 0 is a special case, ignore the first row where T = 0
     print(sealvl_subset[['Time','Sealevel_Uncorrected','Sealevel_Corrected']])
     #calculate range of sea level depth values 
     rangemax = max(sealvl_subset.Sealevel_Corrected[1:])
@@ -16,14 +16,10 @@ def getintervals (sealvl,time,intervals):
     f = open(r"output/intervals.csv","w")
     #write file headers
     f.write("Interval,MinDepth,MaxDepth,MeanDepth,TimeInterval\n")
+    f.write("0,0,0,0,1\n") #set present day sea levels as the first time interval
     #the big nested loop function that calculates the sea level interval bounds and the timespan of each interval
-    for x in range(0,intervals+1,1):
-        if x == 0: #write present-day sea levels as its own line, and calculate number of times sea level has been at current level
-            timeinterval = len(sealvl_subset[sealvl_subset.Sealevel_Corrected == 0])
-            mindepth = 0
-            maxdepth = 0
-            meandepth = 0
-        elif x == 1: #since the first interval has a fixed lower bound, calculate first interval separately
+    for x in range(1,intervals+1,1):
+        if x == 1: #since the first interval has a fixed lower bound, calculate first  interval separately
             #since each line in the input sea level file corresponds with 0.1 kya, timespan can be calculated by counting number of valid rows
             timeinterval = len(sealvl_subset[(sealvl_subset.Sealevel_Corrected <= rangemax) & (sealvl_subset.Sealevel_Corrected >= depthinterval)])
             #calculate lowest sea level depth within interval
